5fcd15ac08a33f73064825b6d28d6b8f6459ac13
Sitam <sitammeur@gmail.com>
2024-10-11 13:40:05 +0530

    yolo11 with tf.js

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+import Loader from "./components/loader";
+import ButtonHandler from "./components/btn-handler";
+import { detect, detectVideo } from "./utils/detect";
+import "./style/App.css";
+
+/**
+ * App component for YOLO11 Live Detection Application.
+ *
+ * This component initializes and loads a YOLO11 model using TensorFlow.js,
+ * sets up references for image, camera, video, and canvas elements, and
+ * handles the loading state and model configuration.
+ */
+
+const App = () => {
+  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  }); // init model & input shape
+
+  // references
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // model configs
+  const modelName = "yolov11n";
+
+  useEffect(() => {
+    tf.ready().then(async () => {
+      const yolov11 = await tf.loadGraphModel(
+        `${window.location.href}/${modelName}_web_model/model.json`,
+        {
+          onProgress: (fractions) => {
+            setLoading({ loading: true, progress: fractions }); // set loading fractions
+          },
+        }
+      ); // load model
+
+      // warming up model
+      const dummyInput = tf.ones(yolov11.inputs[0].shape);
+      const warmupResults = yolov11.execute(dummyInput);
+
+      setLoading({ loading: false, progress: 1 });
+      setModel({
+        net: yolov11,
+        inputShape: yolov11.inputs[0].shape,
+      }); // set model & input shape
+
+      tf.dispose([warmupResults, dummyInput]); // cleanup memory
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {loading.loading && (
+        <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>
+      )}
+      <div className="header">
+        <h1>📷 YOLO11 Live Detection App</h1>
+        <p>
+          YOLO11 live detection application on browser powered by{" "}
+          <code>tensorflow.js</code>
+        </p>
+        <p>
+          Serving : <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      <div className="content">
+        <img
+          src="#"
+          ref={imageRef}
+          onLoad={() => detect(imageRef.current, model, canvasRef.current)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={cameraRef}
+          onPlay={() =>
+            detectVideo(cameraRef.current, model, canvasRef.current)
+          }
+        />
+        <video
+          autoPlay
+          muted
+          ref={videoRef}
+          onPlay={() => detectVideo(videoRef.current, model, canvasRef.current)}
+        />
+        <canvas
+          width={model.inputShape[1]}
+          height={model.inputShape[2]}
+          ref={canvasRef}
+        />
+      </div>
+
+      <ButtonHandler
+        imageRef={imageRef}
+        cameraRef={cameraRef}
+        videoRef={videoRef}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+/**
+ * Entry point of the React application.
+ *
+ * This file sets up the root of the React application and renders the main App component
+ * within a StrictMode wrapper for highlighting potential problems in the application.
+ */
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./style/index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
