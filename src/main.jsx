43f797535db194c3a1ba249742aebeec437e7546
Sitam <sitammeur@gmail.com>
2024-10-12 02:05:38 +0530

    speed insights and web analytics added

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from "./App.jsx";
 import "./style/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
+    <Analytics />
     <App />
   </StrictMode>
 );
